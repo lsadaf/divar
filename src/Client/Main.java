5abdfdb04b882a93ddfd1d292b8eccbb82c9a23e
Shamim Taghizadeh <109668415+ShamimTaghizadeh@users.noreply.github.com>
2023-01-24 00:49:17 +0330

    Create Main.java

---
+++ src/Client/Main.java
@@ -0,0 +1,24 @@
+package Client;
+
+import CLI.ClearScreen;
+import CLI.Colors;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static Socket socket;
+    public static void main(String[] args) throws IOException {
+      /*  try {
+            socket = new Socket("localhost", 4000);
+        } catch (IOException e) {
+            Colors.colorPrint(Colors.RED, "MAKE SURE SERVER IS CONNECTED");
+            System.exit(1);
+        }
+        ActionHandler actionHandler = new ActionHandler(socket);
+        actionHandler.start();*/
+        
+
+    }
+}
