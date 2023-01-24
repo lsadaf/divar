dfe7b86d0c61f271657df10ec7671354c8a8d7f0
Shamim Taghizadeh <109668415+ShamimTaghizadeh@users.noreply.github.com>
2023-01-24 10:12:51 +0330

    Update DataHolder.java

--- src/Server/DataHolder.java
+++ src/Server/DataHolder.java
@@ -1,22 +1,54 @@
 package Server;
 
-import DataTypes.Advertisement;
-import DataTypes.User;
+import Exceptions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DataHolder {
-    ArrayList<User> users;
-    ArrayList<Advertisement> advertisements;
+public class DataBase {
+    private final static String initPath = "";
+    private final static String usersFile = initPath + "users.json";
 
-    public DataHolder() {
-        loadData();
+    private static void saveUsers(ArrayList<User> users) {
+        try (FileWriter fileWriter = new FileWriter(usersFile)){
+            for (User user : users)
+                fileWriter.write(user.toJson() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void loadData (){
-        //load data from files/DB
+    public static ArrayList<User> getUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(usersFile))){
+            while (scanner.hasNextLine())
+                users.add(User.fromJson(scanner.nextLine()));
+        } catch (FileNotFoundException e) {
+            try (FileWriter fileWriter = new FileWriter(usersFile)) {
+                fileWriter.write("");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return users;
     }
-    public void saveData (){
-        //save data to file/DB
+
+    public static void addUser(User user) throws UserAlreadyExists {
+        ArrayList<User> users = getUsers();
+        if (!users.contains(user)){
+            users.add(user);
+        }else throw new UserAlreadyExists("User already exists!");
+        saveUsers(users);
     }
+
+    public static void removeUser(User user) {
+        ArrayList<User> users = getUsers();
+        users.remove(user);
+        saveUsers(users);
+    }
+
 }
