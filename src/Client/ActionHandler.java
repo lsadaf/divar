5f8f36f23dc273856d9b868dc8876b542dbb9425
Shamim Taghizadeh <109668415+ShamimTaghizadeh@users.noreply.github.com>
2023-01-24 17:32:15 +0330

    Update ActionHandler.java

--- src/Client/ActionHandler.java
+++ src/Client/ActionHandler.java
@@ -6,44 +6,56 @@ import Communication.RequestType;
 import Communication.ResponsePacket;
 import DataTypes.Advertisement;
 
+import java.awt.*;
+import java.io.Console;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class ActionHandler {
-    Scanner scanner;
-    Socket socket;
-    ObjectInputStream objectInputStream;
-    ObjectOutputStream objectOutputStream;
+    private final Scanner scanner;
+    private final ObjectInputStream objectInputStream;
+    private final ObjectOutputStream objectOutputStream;
+  //  ArrayList<Advertisement> test;
 
-    public ActionHandler(Socket inputSocket) throws IOException {
-        socket = inputSocket;
+    public ActionHandler(Socket socket) throws IOException {
         objectInputStream = new ObjectInputStream(socket.getInputStream());
         objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         scanner = new Scanner(System.in);
     }
 
+
+    //just for test
+   /* public ActionHandler() {
+        scanner = new Scanner(System.in);
+    }*/
+
+    //just for test
+    /*public void setTest(ArrayList<Advertisement> test) {
+        this.test = test;
+    }*/
+
     public void start (){
         printMenu();
-
     }
 
     public void printMenu (){
         int input;
         ClearScreen.clearScreen();
         while (true){
-            System.out.print("1. PROFILE\n2.ADVERTISEMENT\n3.EXIT>> ");
+            System.out.print("1. PROFILE\n2. ADVERTISEMENT\n3. EXIT\n>> ");
             input = scanner.nextInt();
+            scanner.nextLine();
             if ( input == 1){
+                //todo
 
             }
             else if ( input == 2){
                 advertisementMenu();
-
             }
             else if ( input == 3){
                 ClearScreen.clearScreen();
@@ -65,26 +77,141 @@ public class ActionHandler {
         while(true){
             System.out.print("1. ADVERTISEMENT LIST\n2. ADD NEW ADVERTISEMENT\n3. BACK\n>> ");
             input = scanner.nextInt();
+            scanner.nextLine();
             if ( input == 1){
-                while (true){
                     RequestPacket requestPacket = new RequestPacket(RequestType.GET_ALL_ADS, null);
+                    sendRequest(requestPacket);
                     ResponsePacket responsePacket = getResponse();
-                    PrintLists.printAllAdList((ArrayList<Advertisement>) responsePacket.getInitialData());
-                    System.out.print("1. SORT\n2. PRICE RANGE\n3. BACK>> ");
+                    ArrayList<Advertisement> mainList = (ArrayList<Advertisement>) responsePacket.getInitialData();
+                   // ArrayList<Advertisement> mainList = test; for test
+                    ArrayList<Advertisement> adList = mainList;
+                    PrintLists.printAllAdList(adList);
+                while (true){
+                    System.out.print("1. SORT\n2. FILTER PRICE RANGE\n3. FILTER CITY\n4. SEARCH\n5. VIEW DETAILS\n6. RESET FILTERS\n7. BACK\n>> ");
                     int input1 = scanner.nextInt();
+                    scanner.nextLine();
                     if ( input1 == 1){
+                        while (true){
+                            ClearScreen.clearScreen();
+                            System.out.print("SORT\n-----------\n1. BY PRICE\n2. BY UPDATE DATE\n3. BACK\n>> ");
+                            int input2 = scanner.nextInt();
+                            scanner.nextLine();
+                            if ( input2 == 1){
+                                System.out.print("1. ASCENDING\n2. DESCENDING\n>> ");
+                                int input3 = scanner.nextInt();
+                                scanner.nextLine();
+                                ClearScreen.clearScreen();
+                                if ( input3 == 1){
+                                    PrintLists.printSortedByPrice(adList, true);
+                                    break;
+                                }
+                                else if (input3 == 2){
+                                    PrintLists.printSortedByPrice(adList, false);
+                                    break;
+                                }
+                            }
+                            else if ( input2 == 2){
+                                ClearScreen.clearScreen();
+                                PrintLists.printAdByUploadDate(adList);
+                                break;
+                            }
+                            else if ( input2 == 3){
+                                ClearScreen.clearScreen();
+                                PrintLists.printAllAdList(adList);
+                                break;
+                            }
+                        }
 
                     }
                     else if ( input1 == 2){
+                        ClearScreen.clearScreen();
+                        System.out.print("PRICE RANGE\n" + Colors.colorPrint(Colors.CYAN, "-----------") + "\nENTER PRICE 1\n>> ");
+                        int num1 = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("ENTER PRICE 2\n>> ");
+                        int num2 = scanner.nextInt();
+                        scanner.nextLine();
+                        ClearScreen.clearScreen();
+                        PrintLists.printAdByPriceRange(adList, num1, num2);
+                    }
+                    else if ( input1 == 3){
+                        ClearScreen.clearScreen();
+                        System.out.print("ENTER CITY\n>> ");
+                        String string = scanner.nextLine();
+                        ClearScreen.clearScreen();
+                        PrintLists.printAdByCity(adList, string.toUpperCase());
+                    }
+                    else if ( input1 == 4){
+                        ClearScreen.clearScreen();
+                        System.out.print("SEARCH NAME\n>> ");
+                        String string = scanner.nextLine();
+                        ClearScreen.clearScreen();
+                        PrintLists.printAdBySearch(adList, string);
 
                     }
-                    else if (input1 == 3){
+                    else if ( input1 == 5){
+                        System.out.print("ENTER ADS ID\n>> ");
+                        String ads_id = scanner.nextLine();
+                        ClearScreen.clearScreen();
+                        if ( PrintLists.printAdDetails(adList, ads_id)){
+                            System.out.print("1. ADD TO FAVORITES\n2. BACK\n>> ");
+                            int input4 = scanner.nextInt();
+                            scanner.nextLine();
+                            if ( input4 == 1){
+                                RequestPacket requestPacket1 = new RequestPacket(RequestType.ADD_AD_TO_FAVORITES, ads_id);
+                                sendRequest(requestPacket1);
+                            }
+                        }
+                        ClearScreen.clearScreen();
+                        PrintLists.printAllAdList(adList);
+                    }
+                    else if ( input1 == 6){
+                        adList = mainList;
+                        ClearScreen.clearScreen();
+                        PrintLists.printAllAdList(adList);
+
+                    }
+                    else if (input1 == 7){
+                        ClearScreen.clearScreen();
                         break;
                     }
                 }
             }
             else if ( input == 2){
-
+                System.out.println("ADD NEW ADVERTISEMENT\n" + Colors.colorPrint(Colors.CYAN,"--------"));
+                String name, city, description, location, phone;
+                double price;
+                System.out.print("ENTER ADS NAME\n>> ");
+                name = scanner.nextLine();
+                System.out.print("ENTER ADS DESCRIPTION\n>> ");
+                description = scanner.nextLine();
+                System.out.print("ENTER ITEMS PRICE\n>> ");
+                price = Double.parseDouble(scanner.nextLine());
+                System.out.print("ENTER YOUR CITY\n>> ");
+                city = scanner.nextLine();
+                System.out.print("ENTER YOUR ADDRESS\n>> ");
+                location = scanner.nextLine();
+                System.out.print("ENTER YOUR NUMBER\n>> ");
+                phone = scanner.nextLine();
+                int numOfPics;
+                System.out.print("ENTER NUMBER OF PICS YOU WANT TO CHOOSE\n>> ");
+                numOfPics = scanner.nextInt();
+                scanner.nextLine();
+                String[] pics = new String[numOfPics];
+                for ( int i = 0 ; i < numOfPics ; i++){
+                    FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+                    dialog.setMode(FileDialog.LOAD);
+                    dialog.setVisible(true);
+                    String file = dialog.getFile();
+                    dialog.dispose();
+                    pics[i] = file;
+                }
+                Advertisement advertisement = new Advertisement(name, description, price, city, location, phone, LocalDateTime.now(),pics, false);
+                RequestPacket requestPacket = new RequestPacket(RequestType.NEW_AD, advertisement);
+                sendRequest(requestPacket);
+                //test.add(advertisement); for test
+                ClearScreen.clearScreen();
+                System.out.println(Colors.colorPrint(Colors.GREEN_BRIGHT, "ADVERTISEMENT WAS CREATED SUCCESSFULLY"));
             }
             else if ( input == 3){
                 ClearScreen.clearScreen();
@@ -109,8 +236,10 @@ public class ActionHandler {
         ResponsePacket responsePacket = null;
         try {
             responsePacket = (ResponsePacket) objectInputStream.readUnshared();
-        } catch (IOException | ClassNotFoundException ignored) {
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
         return responsePacket;
     }
