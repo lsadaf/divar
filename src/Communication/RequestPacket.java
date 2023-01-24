454792d24c07c1b7124000805856e439b3ba7bc3
Shamim Taghizadeh <109668415+ShamimTaghizadeh@users.noreply.github.com>
2023-01-24 17:51:24 +0330

    Update RequestPacket.java

--- src/Communication/RequestPacket.java
+++ src/Communication/RequestPacket.java
@@ -3,11 +3,19 @@ package Communication;
 import java.io.Serializable;
 
 public class RequestPacket implements Serializable{
-    Object data;
-    RequestType requestType;
+    private final Object data;
+    private final RequestType requestType;
 
     public RequestPacket(RequestType requestType, Object data) {
         this.data = data;
         this.requestType = requestType;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public RequestType getRequestType() {
+        return requestType;
     }
 }
